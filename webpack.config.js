59a1d2652f97ba799ca993763d8d0f994d166a75
Michael Friedrich <m.friedrich3@yahoo.com>
2022-12-27 14:45:57 +0100

    install html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -12,4 +13,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ],
 };
